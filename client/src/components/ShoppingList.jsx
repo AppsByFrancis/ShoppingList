ad4bec3989d0ec7a3aa312620baa1be20f246427
AppsByFrancis <majer.frantisek@hotmail.com>
2023-11-26 23:50:35 +0100

    delete a list

--- client/src/components/ShoppingList.jsx
+++ client/src/components/ShoppingList.jsx
@@ -3,18 +3,26 @@ import { ShoppingListContext } from '../App';
 import { Link } from 'react-router-dom';
 import './css/shoppingList.css'
 
+
 const ShoppingList = () => {
-    const [shoppingList] = useContext(ShoppingListContext);
+    const [shoppingList, setShoppingList] = useContext(ShoppingListContext);
     const colors = ["red", "blue", "green", "orange", "dark-blue", "teal", "pink", "yellow", "purple"]
 
+    const removeItem = (itemId) => {
+        const filteredList = shoppingList.filter(item => item.id !== itemId);
 
+        setShoppingList(filteredList);
+    };
     return(
         <section className='shoppingListSection'> 
            {shoppingList && shoppingList.map( item => {
                 return(
-                    <Link to={`/list/${item.id}`} className={`shoppingList ${colors[item.index]}`} key={item.id}>
-                        <h1>{item.name}</h1>
-                    </Link>
+                    <div key={item.id} className={`shoppingList ${colors[item.index]}`}>
+                        <button onClick={() => {removeItem(item.id)}}>Remove</button>
+                        <Link to={`/list/${item.id}`} key={item.id}>
+                            <h1>{item.name}</h1>
+                        </Link>
+                    </div>
                 )
            })}
         </section>
